2dbdf2f618d2ef7b5b66d1e402947ab787a8a402
LidiaStrelets <0630610171li@gmail.com>
2021-04-14 14:11:03 +0300

    supporting lazy loading

--- js/r2-t2.js
+++ js/r2-t2.js
@@ -49,35 +49,41 @@ const images = [
   },
 ];
 
-const createMenu = dishes =>
-  dishes
-    .map(
-      ({ src, alt, width, height }) => `<li class="dish">
-  <img loading="lazy"
-  src="${src}" alt="${alt}" width=${width} height=${height} />
-  <p class="dish__description">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quod
-    pariatur, maxime ut error aperiam cupiditate provident adipisci, vel
-    deserunt enim doloremque qui laudantium optio est magni id mollitia eum!
-  </p>
-</li>`,
-    )
-    .join('');
+if ('loading' in HTMLImageElement.prototype) {
+  console.log('I support!');
+} else {
+  console.log('And me dont');
+}
 
-const refs = {
-  list: document.querySelector('.menu'),
-};
+// const createMenu = dishes =>
+//   dishes
+//     .map(
+//       ({ src, alt, width, height }) => `<li class="dish">
+//   <img loading="lazy"
+//   src="${src}" alt="${alt}" width=${width} height=${height} />
+//   <p class="dish__description">
+//     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quod
+//     pariatur, maxime ut error aperiam cupiditate provident adipisci, vel
+//     deserunt enim doloremque qui laudantium optio est magni id mollitia eum!
+//   </p>
+// </li>`,
+//     )
+//     .join('');
 
-const menu = createMenu(images);
-refs.list.innerHTML = menu;
+// const refs = {
+//   list: document.querySelector('.menu'),
+// };
 
-const imagesRef = document.querySelectorAll('[loading="lazy"]');
+// const menu = createMenu(images);
+// refs.list.innerHTML = menu;
 
-const handleImageLoading = event => {
-  console.log('Loaded image', event.target);
-  event.target.classList.add('loaded');
-};
+// const imagesRef = document.querySelectorAll('[loading="lazy"]');
 
-imagesRef.forEach(img => {
-  img.addEventListener('load', handleImageLoading, { once: true });
-});
+// const handleImageLoading = event => {
+//   console.log('Loaded image', event.target);
+//   event.target.classList.add('loaded');
+// };
+
+// imagesRef.forEach(img => {
+//   img.addEventListener('load', handleImageLoading, { once: true });
+// });
